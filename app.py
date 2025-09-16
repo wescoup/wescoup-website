e08c3ac01b56d294d283b05a18f21417173f4dae
wescoup <48031986+wescoup@users.noreply.github.com>
2025-09-16 12:00:04 -0500

    Update app.py

--- app.py
+++ app.py
@@ -9,33 +9,33 @@ def home():
 
 @app.route('/tonys-tennis-page')
 def tonys_tennis_page():
-    return render_template('tonys-tennis-page.html')
+    return render_template('tennis/tonys-tennis-page.html')
 
 @app.route('/tonys-tennis-tracker')
 def tonys_tennis_tracker():
-    return render_template('tonys-tennis-tracker.html')
+    return render_template('tennis/tonys-tennis-tracker.html')
 
 @app.route('/tonys-doubles-tracker')
 def tonys_doubles_tracker():
-    return render_template('tonys-doubles-tracker.html')
+    return render_template('tennis/tonys-doubles-tracker.html')
 
 @app.route('/second-shot')
 def second_shot():
-    return render_template('second-shot.html')
+    return render_template('tennis/second-shot.html')
 
 @app.route('/tonys-tennis-tracker-20')
 def tonys_tennis_tracker_20():
-    return render_template('tonys-tennis-tracker-20.html')
+    return render_template('tennis/tonys-tennis-tracker-20.html')
 
 @app.route("/tonys-strategy-calculator")
 def tonys_strategy_calculator():
     """Show Tony's Strategy Calculator page"""
-    return render_template("tonys-strategy-calculator.html")
+    return render_template("tennis/tonys-strategy-calculator.html")
 
 @app.route("/tonys-strategy-calculator-instructions")
 def tonys_strategy_calculator_instructions():
     """Show Tony's Strategy Calculator instructions page"""
-    return render_template("tonys-strategy-calculator-instructions.html")
+    return render_template("tennis/tonys-strategy-calculator-instructions.html")
 
 @app.route("/testpage")
 def testpage():
